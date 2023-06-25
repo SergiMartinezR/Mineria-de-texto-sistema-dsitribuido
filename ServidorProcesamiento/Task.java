8c7da8384153d78d99f4c25c391fe3b0218f5efd
Sergi Martinez <sergimartinezr7@gmail.com>
2023-06-25 14:08:00 -0600

    Se agregó la clase Task que contendra la tarea que tiene que realizar cada servidor de procesamiento

---
+++ ServidorProcesamiento/Task.java
@@ -0,0 +1,23 @@
+public class Task implements java.io.Serializable {
+  private int minLim;
+  private int maxLim;
+  private String frase;
+
+  public Task(int minLim, int maxLim, String frase) {
+    this.minLim = minLim;
+    this.maxLim = maxLim;
+    this.frase = frase;
+  }
+
+  public int obtenerMinLim() {
+    return this.minLim;
+  }
+
+  public int obtenerMaxLim() {
+    return this.maxLim;
+  }
+
+  public String obtenerFrase() {
+    return this.frase;
+  }
+}
